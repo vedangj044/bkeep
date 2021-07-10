36cbd7cfe16d89b20127c00a124b8deaceed0625
vedangj044 <vedangj044@gmail.com>
2021-07-10 14:26:59 +0530

    Initial database setup

---
+++ migrations/2021-07-10-083042_book_table/up.sql
@@ -0,0 +1,7 @@
+-- Your SQL goes here
+CREATE TABLE book_table (
+    id INT NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    dtime DATE NOT NULL,
+    PRIMARY KEY (id)
+);
---
+++ src/schema.rs
@@ -0,0 +1,7 @@
+table! {
+    book_table (id) {
+        id -> Integer,
+        title -> Varchar,
+        dtime -> Date,
+    }
+}
